272c6246166761ea5c00ae57b501cd44e863d981
theawesomeguy111 <elliotliskin@gwmail.gwu.edu>
2015-12-11 19:23:29 -0500

    Added Commented Code and Note for Achieving Layout
    -Added some commented out code that doesn't quite achieve the desired layout
    -Added notes for how to correctly achieve the desired layout
    -Added several more test views
    -Fixed some of the other test views
    -Various micro-optimizations

--- ClassExtractor App/ClassExtractor/CEWordCloudViewController.m
+++ ClassExtractor App/ClassExtractor/CEWordCloudViewController.m
@@ -8,6 +8,7 @@
 
 #import "CEWordCloudViewController.h"
 #import "CECloudView.h"
+#include <math.h>
 
 // ============================================================
 // CEWordCloudViewController
@@ -77,7 +78,7 @@
     [topic7 setTopicName: @"Australia's Economy"];
     [topic7 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(602, 1), CMTimeMake(710, 1))];
     [topic7 setImportanceWeighting: 70];
-    CECloudView* cloudView7 = [[CECloudView alloc] initWithTopic: topic6];
+    CECloudView* cloudView7 = [[CECloudView alloc] initWithTopic: topic7];
     [view addSubview: cloudView7];
     [views addObject: cloudView7];
     
@@ -85,7 +86,7 @@
     [topic8 setTopicName: @"The Fed"];
     [topic8 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(614, 1), CMTimeMake(971, 1))];
     [topic8 setImportanceWeighting: 34];
-    CECloudView* cloudView8 = [[CECloudView alloc] initWithTopic: topic6];
+    CECloudView* cloudView8 = [[CECloudView alloc] initWithTopic: topic8];
     [view addSubview: cloudView8];
     [views addObject: cloudView8];
     
@@ -93,17 +94,114 @@
     [topic9 setTopicName: @"Banks"];
     [topic9 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(412, 1), CMTimeMake(971, 1))];
     [topic9 setImportanceWeighting: 100];
-    CECloudView* cloudView9 = [[CECloudView alloc] initWithTopic: topic6];
+    CECloudView* cloudView9 = [[CECloudView alloc] initWithTopic: topic9];
     [view addSubview: cloudView9];
     [views addObject: cloudView9];
     
     CETopic* topic10 = [[CETopic alloc] init];
     [topic10 setTopicName: @"Crowding Out"];
     [topic10 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
-    [topic10 setImportanceWeighting: 18];
-    CECloudView* cloudView10 = [[CECloudView alloc] initWithTopic: topic6];
+    [topic10 setImportanceWeighting: 54];
+    CECloudView* cloudView10 = [[CECloudView alloc] initWithTopic: topic10];
     [view addSubview: cloudView10];
     [views addObject: cloudView10];
+    
+    CETopic* topic11 = [[CETopic alloc] init];
+    [topic11 setTopicName: @"Crowding Out"];
+    [topic11 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic11 setImportanceWeighting: 47];
+    CECloudView* cloudView11 = [[CECloudView alloc] initWithTopic: topic11];
+    [view addSubview: cloudView11];
+    [views addObject: cloudView11];
+    
+    CETopic* topic12 = [[CETopic alloc] init];
+    [topic12 setTopicName: @"Crowding Out"];
+    [topic12 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic12 setImportanceWeighting: 39];
+    CECloudView* cloudView12 = [[CECloudView alloc] initWithTopic: topic12];
+    [view addSubview: cloudView12];
+    [views addObject: cloudView12];
+    
+    CETopic* topic13 = [[CETopic alloc] init];
+    [topic13 setTopicName: @"Crowding Out"];
+    [topic13 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic13 setImportanceWeighting: 22];
+    CECloudView* cloudView13 = [[CECloudView alloc] initWithTopic: topic13];
+    [view addSubview: cloudView13];
+    [views addObject: cloudView13];
+    
+    CETopic* topic14 = [[CETopic alloc] init];
+    [topic14 setTopicName: @"Crowding Out"];
+    [topic14 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic14 setImportanceWeighting: 67];
+    CECloudView* cloudView15 = [[CECloudView alloc] initWithTopic: topic14];
+    [view addSubview: cloudView15];
+    [views addObject: cloudView15];
+    
+    CETopic* topic16 = [[CETopic alloc] init];
+    [topic16 setTopicName: @"Crowding Out"];
+    [topic16 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic16 setImportanceWeighting: 51];
+    CECloudView* cloudView16 = [[CECloudView alloc] initWithTopic: topic16];
+    [view addSubview: cloudView16];
+    [views addObject: cloudView16];
+    
+    CETopic* topic17 = [[CETopic alloc] init];
+    [topic17 setTopicName: @"Crowding Out"];
+    [topic17 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic17 setImportanceWeighting: 99];
+    CECloudView* cloudView17 = [[CECloudView alloc] initWithTopic: topic17];
+    [view addSubview: cloudView17];
+    [views addObject: cloudView17];
+    
+    CETopic* topic18 = [[CETopic alloc] init];
+    [topic18 setTopicName: @"Crowding Out"];
+    [topic18 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic18 setImportanceWeighting: 87];
+    CECloudView* cloudView18 = [[CECloudView alloc] initWithTopic: topic18];
+    [view addSubview: cloudView18];
+    [views addObject: cloudView18];
+    
+    CETopic* topic19 = [[CETopic alloc] init];
+    [topic19 setTopicName: @"Crowding Out"];
+    [topic19 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic19 setImportanceWeighting: 20];
+    CECloudView* cloudView19 = [[CECloudView alloc] initWithTopic: topic19];
+    [view addSubview: cloudView19];
+    [views addObject: cloudView19];
+    
+    CETopic* topic20 = [[CETopic alloc] init];
+    [topic20 setTopicName: @"Crowding Out"];
+    [topic20 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic20 setImportanceWeighting: 6];
+    CECloudView* cloudView20 = [[CECloudView alloc] initWithTopic: topic20];
+    [view addSubview: cloudView20];
+    [views addObject: cloudView20];
+    
+    CETopic* topic21 = [[CETopic alloc] init];
+    [topic21 setTopicName: @"Crowding Out"];
+    [topic21 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic21 setImportanceWeighting: 59];
+    CECloudView* cloudView21 = [[CECloudView alloc] initWithTopic: topic21];
+    [view addSubview: cloudView21];
+    [views addObject: cloudView21];
+    
+    CETopic* topic22 = [[CETopic alloc] init];
+    [topic22 setTopicName: @"Crowding Out"];
+    [topic22 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic22 setImportanceWeighting: 62];
+    CECloudView* cloudView22 = [[CECloudView alloc] initWithTopic: topic22];
+    [view addSubview: cloudView22];
+    [views addObject: cloudView22];
+    
+    CETopic* topic23 = [[CETopic alloc] init];
+    [topic23 setTopicName: @"Crowding Out"];
+    [topic23 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(110, 1), CMTimeMake(212, 1))];
+    [topic23 setImportanceWeighting: 71];
+    CECloudView* cloudView23 = [[CECloudView alloc] initWithTopic: topic23];
+    [view addSubview: cloudView23];
+    [views addObject: cloudView23];
+    
     
     // [TODO] Test what happens if there are two topics of the same importance.
     NSArray* sortedViews = [self orderViewsByImportance: views];
@@ -159,20 +257,20 @@
 // ------------------------------------------------------------
 // layoutCloudsFromArray:
 // ------------------------------------------------------------
-- (void) layoutCloudsFromArray: (NSArray<CECloudView*>*)views
-{
-    NSUInteger totalX = 0;
-    const NSUInteger viewCount = [views count];
-    
-    for (NSUInteger i = 0; i < viewCount; ++i)
-    {
-        CECloudView* curCloud = [views objectAtIndex: i];
-        const NSSize curCloudSize = [curCloud frame].size;
-        [curCloud setFrame: CGRectMake(totalX, 0, curCloudSize.width, curCloudSize.height)];
-        
-        totalX += curCloudSize.width;
-    }
-}
+//- (void) layoutCloudsFromArray: (NSArray<CECloudView*>*)views
+//{
+//    NSUInteger totalX = 0;
+//    const NSUInteger viewCount = [views count];
+//    
+//    for (NSUInteger i = 0; i < viewCount; ++i)
+//    {
+//        CECloudView* curCloud = [views objectAtIndex: i];
+//        const NSSize curCloudSize = [curCloud frame].size;
+//        [curCloud setFrame: CGRectMake(totalX, 0, curCloudSize.width, curCloudSize.height)];
+//        
+//        totalX += curCloudSize.width;
+//    }
+//}
 
 
 // ------------------------------------------------------------
@@ -183,31 +281,30 @@
 // ------------------------------------------------------------
 - (void) layoutCloudsFromArrayNew: (NSArray<CECloudView*>*)views
 {
-    for (NSUInteger i = 0; i < [views count]; ++i)
+    NSUInteger numViews = [views count];
+    
+    for (NSUInteger i = 0; i < numViews; ++i)
     {
         CECloudView* view = [views objectAtIndex: i];
-        const CGPoint center = [self calculateCenterOfCloudWithIndex: i withView: view];
-        const CGSize size = [view frame].size;
-        [view setFrame: CGRectMake(center.x, center.y, size.width, size.height)];
+        const CGFloat diameter = [view frame].size.width; // the view is a circle, so the width and height are equal
+        const CGPoint center = [self calculateCenterOfCloudWithIndex: i withViewDiameter: diameter];
+        [view setFrame: CGRectMake(center.x, center.y, diameter, diameter)];
     }
 }
 
 
 // ------------------------------------------------------------
 // calculateStartIndexForLevel:
+//
+// Returns the starting index of a given level. This index is
+// the first index of this level (so the cloud at this level is
+// *on* this level).
 // ------------------------------------------------------------
 - (NSUInteger) calculateStartIndexForLevel: (NSUInteger)level
 {
-    NSUInteger startingIndex;
-    
-    if (level == 0)
-        startingIndex = 0;
-    else if (level % 2 == 0)
-        startingIndex = level * level + 1;
-    else
-        startingIndex = level * level;
-    
-    return startingIndex;
+    // The starting index is determined by the level squared
+    // for even levels and the level squared plus one for odd levels.
+    return level * level + (level % 2 == 0);
 }
 
 
@@ -225,15 +322,14 @@
 //
 // Levels are zero offset.
 // ------------------------------------------------------------
-- (NSUInteger) getLevelForNum: (NSUInteger)num
+- (NSUInteger) getLevelForIndex: (NSUInteger)index
 {
     NSUInteger curLevel = 0;
     bool foundLevel = false;
     
     while (!foundLevel)
     {
-        foundLevel = [self calculateStartIndexForLevel: curLevel+1] > num;
-        ++curLevel;
+        foundLevel = [self calculateStartIndexForLevel: ++curLevel] > index;
     }
     
     return curLevel-1;
@@ -245,7 +341,7 @@
 // ------------------------------------------------------------
 - (NSUInteger) numLevelsForNumClouds: (NSUInteger)numClouds
 {
-    return [self getLevelForNum: numClouds];
+    return [self getLevelForIndex: numClouds];
 }
 
 
@@ -270,7 +366,7 @@
 // ------------------------------------------------------------
 - (NSUInteger) quadrantForCloudIndex: (NSUInteger)cloudIndex
 {
-    const NSUInteger level = [self getLevelForNum: cloudIndex];
+    const NSUInteger level = [self getLevelForIndex: cloudIndex];
     const NSUInteger cloudsPerQuadrant = [self totalCloudsPerQuadrantForLevel: level];
     
     if (cloudsPerQuadrant == 0)
@@ -288,15 +384,57 @@
 // index is the index of this cloud in the array if ordered
 // clouds.
 // ------------------------------------------------------------
-- (CGPoint) calculateCenterOfCloudWithIndex: (NSUInteger)index withView: (CECloudView*)cloudView
+- (CGPoint) calculateCenterOfCloudWithIndex: (NSUInteger)index withViewDiameter: (CGFloat)curCloudDiameter
 {
-    const NSUInteger levelMultiplier = 150;
-    const NSUInteger curLevel = [self getLevelForNum: index];
+    const NSUInteger levelMultiplier = 300;
+    const NSUInteger curLevel = [self getLevelForIndex: index];
     const NSUInteger curQuadrant = [self quadrantForCloudIndex: index];
     
+    CGFloat xPos = 0;
+    CGFloat yPos = 0;
+    if (index <= 4 && index > 0)
+    {
+        // [TODO] These should be calculated such that the circles are all the same distance from touching the central circle
+        switch (index) {
+            case 1:
+                yPos += 200;
+                break;
+            case 2:
+                xPos += 200;
+                break;
+            case 3:
+                yPos += 200;
+                break;
+            default: // 4
+                xPos += 200;
+                break;
+        }
+    }
+    else if (index > 4)
+    {
+        // FIRST QUADRANT:
+        // radius should the radius of the circle created by the two circles it should be between
+            // need some way of getting the two circles it should be between
+        // radius can be calculated by taking the difference of the two x positions of the circle's centers
+        // :::in the first quadrant, the center of the circle can be found by adding the radius to the y of the lower circle.
+        // center of cloud should be center of circle
+        // Add some small constant to the x and y of center to push cloud out a little bit to prevent clouds from overlapping
+            // Really, should figure out if they do overlap, and if they do, push the new cloud out just enough so they don't overlap
+//        const NSUInteger numCloudsOnCurLevel = [self numCloudsForLevel: curLevel];
+//        const NSUInteger anglesCreatedByClouds = numCloudsOnCurLevel;
+//        const NSUInteger degreesPerCloud = 90 / anglesCreatedByClouds;
+//        const CGFloat radians = degreesPerCloud * M_PI / 180;
+//        const NSUInteger curLevelDiameter = curLevel * levelMultiplier;
+//        xPos = cos(radians) * curLevelDiameter / 2;
+//        yPos = sin(radians) * curLevelDiameter / 2;
+    }
     // [TODO] Account for positions within a quadrant per level, instead of just the level itself.
-    NSInteger y = curLevel * levelMultiplier;
-    NSInteger x = curLevel * levelMultiplier;
+    // [TODO] Tighten the spiral; the varying sizes of the circles makes it so we don't currently
+    // take into account the differently sized spaces between the circles of different levels.
+//    NSInteger x = xPos + curLevel * levelMultiplier;
+//    NSInteger y = yPos + curLevel * levelMultiplier;
+    NSInteger x = xPos;
+    NSInteger y = yPos;
     
     if (curQuadrant == 1)
         y = -y;
@@ -308,17 +446,22 @@
     else if (curQuadrant == 3)
         x = -x;
     
-    const NSUInteger originOffset = 250;
-    y += originOffset;
+    const NSUInteger originOffset = 600;
+    y += 250;
     x += originOffset;
     
-    // the views are circles, so the width and height are the same
-    const NSUInteger cloudCenterOffset = [cloudView frame].size.width / 2;
-    y -= cloudCenterOffset;
-    x -= cloudCenterOffset;
+    const NSUInteger radiusOffset = curCloudDiameter / 2;
+    y -= radiusOffset;
+    x -= radiusOffset;
 
-    
     return CGPointMake(x, y);
 }
+
+
+//- (CGPoint) calculateExactPoint: (NSUInteger)index
+//{
+//    
+//    return CGPointMake(0, 0);
+//}
 
 @end
