0dab4480dbb4b82bd8e8c3ef882ec199a56b3d2d
theawesomeguy111 <elliotliskin@gwmail.gwu.edu>
2015-12-12 14:56:30 -0500

    Generate Clouds Based on User Input
    -Everything is all hooked up for the demo, with a few minor issues:
    	-Clouds are way too small
    	-Clouds are in reverse order

--- ClassExtractor App/ClassExtractor/CEWordCloudViewController.m
+++ ClassExtractor App/ClassExtractor/CEWordCloudViewController.m
@@ -28,81 +28,81 @@
                                                  name: kCloudWindowOpened
                                                object: nil];
     
-    NSMutableArray* views = [[NSMutableArray alloc] init];
-    NSView* view = [self view];
-    
-    // [TODO] This is test code, remove this later.
-    CETopic* topic1 = [[CETopic alloc] init];
-    [topic1 setTopicName: @"Marginal Benefit"];
-    [topic1 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(100, 1), CMTimeMake(417, 1))];
-    [topic1 setImportanceWeighting: 77];
-    CECloudView* cloudView1 = [[CECloudView alloc] initWithTopic: topic1];
-    [view addSubview: cloudView1];
-    [views addObject: cloudView1];
-    
-    CETopic* topic2 = [[CETopic alloc] init];
-    [topic2 setTopicName: @"Supply Curve"];
-    [topic2 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(0, 1), CMTimeMake(110, 1))];
-    [topic2 setImportanceWeighting: 46];
-    CECloudView* cloudView2 = [[CECloudView alloc] initWithTopic: topic2];
-    [view addSubview: cloudView2];
-    [views addObject: cloudView2];
-    
-    CETopic* topic3 = [[CETopic alloc] init];
-    [topic3 setTopicName: @"Demand Curve"];
-    [topic3 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(28, 1), CMTimeMake(110, 1))];
-    [topic3 setImportanceWeighting: 45];
-    CECloudView* cloudView3 = [[CECloudView alloc] initWithTopic: topic3];
-    [view addSubview: cloudView3];
-    [views addObject: cloudView3];
-    
-    CETopic* topic4 = [[CETopic alloc] init];
-    [topic4 setTopicName: @"Marginal Cost"];
-    [topic4 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(402, 1), CMTimeMake(496, 1))];
-    [topic4 setImportanceWeighting: 98];
-    CECloudView* cloudView4 = [[CECloudView alloc] initWithTopic: topic4];
-    [view addSubview: cloudView4];
-    [views addObject: cloudView4];
-    
-    CETopic* topic5 = [[CETopic alloc] init];
-    [topic5 setTopicName: @"Edgeworth's Box"];
-    [topic5 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(450, 1), CMTimeMake(512, 1))];
-    [topic5 setImportanceWeighting: 67];
-    CECloudView* cloudView5 = [[CECloudView alloc] initWithTopic: topic5];
-    [view addSubview: cloudView5];
-    [views addObject: cloudView5];
-
-    CETopic* topic6 = [[CETopic alloc] init];
-    [topic6 setTopicName: @"GDP"];
-    [topic6 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(513, 1), CMTimeMake(604, 1))];
-    [topic6 setImportanceWeighting: 52];
-    CECloudView* cloudView6 = [[CECloudView alloc] initWithTopic: topic6];
-    [view addSubview: cloudView6];
-    [views addObject: cloudView6];
-    
-    CETopic* topic7 = [[CETopic alloc] init];
-    [topic7 setTopicName: @"Australia's Economy"];
-    [topic7 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(602, 1), CMTimeMake(710, 1))];
-    [topic7 setImportanceWeighting: 70];
-    CECloudView* cloudView7 = [[CECloudView alloc] initWithTopic: topic7];
-    [view addSubview: cloudView7];
-    [views addObject: cloudView7];
-    
-    CETopic* topic8 = [[CETopic alloc] init];
-    [topic8 setTopicName: @"The Fed"];
-    [topic8 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(614, 1), CMTimeMake(971, 1))];
-    [topic8 setImportanceWeighting: 44];
-    CECloudView* cloudView8 = [[CECloudView alloc] initWithTopic: topic8];
-    [view addSubview: cloudView8];
-    [views addObject: cloudView8];
-    
-    CETopic* topic9 = [[CETopic alloc] init];
-    [topic9 setTopicName: @"Banks"];
-    [topic9 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(412, 1), CMTimeMake(971, 1))];
-    [topic9 setImportanceWeighting: 100];
-    CECloudView* cloudView9 = [[CECloudView alloc] initWithTopic: topic9];
-    [view addSubview: cloudView9];
-    [views addObject: cloudView9];
+//    NSMutableArray* views = [[NSMutableArray alloc] init];
+//    NSView* view = [self view];
+//    
+//    // [TODO] This is test code, remove this later.
+//    CETopic* topic1 = [[CETopic alloc] init];
+//    [topic1 setTopicName: @"Marginal Benefit"];
+//    [topic1 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(100, 1), CMTimeMake(417, 1))];
+//    [topic1 setImportanceWeighting: 77];
+//    CECloudView* cloudView1 = [[CECloudView alloc] initWithTopic: topic1];
+//    [view addSubview: cloudView1];
+//    [views addObject: cloudView1];
+//    
+//    CETopic* topic2 = [[CETopic alloc] init];
+//    [topic2 setTopicName: @"Supply Curve"];
+//    [topic2 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(0, 1), CMTimeMake(110, 1))];
+//    [topic2 setImportanceWeighting: 46];
+//    CECloudView* cloudView2 = [[CECloudView alloc] initWithTopic: topic2];
+//    [view addSubview: cloudView2];
+//    [views addObject: cloudView2];
+//    
+//    CETopic* topic3 = [[CETopic alloc] init];
+//    [topic3 setTopicName: @"Demand Curve"];
+//    [topic3 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(28, 1), CMTimeMake(110, 1))];
+//    [topic3 setImportanceWeighting: 45];
+//    CECloudView* cloudView3 = [[CECloudView alloc] initWithTopic: topic3];
+//    [view addSubview: cloudView3];
+//    [views addObject: cloudView3];
+//    
+//    CETopic* topic4 = [[CETopic alloc] init];
+//    [topic4 setTopicName: @"Marginal Cost"];
+//    [topic4 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(402, 1), CMTimeMake(496, 1))];
+//    [topic4 setImportanceWeighting: 98];
+//    CECloudView* cloudView4 = [[CECloudView alloc] initWithTopic: topic4];
+//    [view addSubview: cloudView4];
+//    [views addObject: cloudView4];
+//    
+//    CETopic* topic5 = [[CETopic alloc] init];
+//    [topic5 setTopicName: @"Edgeworth's Box"];
+//    [topic5 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(450, 1), CMTimeMake(512, 1))];
+//    [topic5 setImportanceWeighting: 67];
+//    CECloudView* cloudView5 = [[CECloudView alloc] initWithTopic: topic5];
+//    [view addSubview: cloudView5];
+//    [views addObject: cloudView5];
+//
+//    CETopic* topic6 = [[CETopic alloc] init];
+//    [topic6 setTopicName: @"GDP"];
+//    [topic6 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(513, 1), CMTimeMake(604, 1))];
+//    [topic6 setImportanceWeighting: 52];
+//    CECloudView* cloudView6 = [[CECloudView alloc] initWithTopic: topic6];
+//    [view addSubview: cloudView6];
+//    [views addObject: cloudView6];
+//    
+//    CETopic* topic7 = [[CETopic alloc] init];
+//    [topic7 setTopicName: @"Australia's Economy"];
+//    [topic7 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(602, 1), CMTimeMake(710, 1))];
+//    [topic7 setImportanceWeighting: 70];
+//    CECloudView* cloudView7 = [[CECloudView alloc] initWithTopic: topic7];
+//    [view addSubview: cloudView7];
+//    [views addObject: cloudView7];
+//    
+//    CETopic* topic8 = [[CETopic alloc] init];
+//    [topic8 setTopicName: @"The Fed"];
+//    [topic8 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(614, 1), CMTimeMake(971, 1))];
+//    [topic8 setImportanceWeighting: 44];
+//    CECloudView* cloudView8 = [[CECloudView alloc] initWithTopic: topic8];
+//    [view addSubview: cloudView8];
+//    [views addObject: cloudView8];
+//    
+//    CETopic* topic9 = [[CETopic alloc] init];
+//    [topic9 setTopicName: @"Banks"];
+//    [topic9 setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(412, 1), CMTimeMake(971, 1))];
+//    [topic9 setImportanceWeighting: 100];
+//    CECloudView* cloudView9 = [[CECloudView alloc] initWithTopic: topic9];
+//    [view addSubview: cloudView9];
+//    [views addObject: cloudView9];
     
 //    CETopic* topic10 = [[CETopic alloc] init];
 //    [topic10 setTopicName: @"Crowding Out"];
@@ -210,13 +210,14 @@
     
     
     // [TODO] Test what happens if there are two topics of the same importance.
-    NSArray* sortedViews = [self orderViewsByImportance: views];
-    NSUInteger totalWidthNeeded = [self calculateTotalWidthFromViews: sortedViews];
+//    NSArray* sortedViews = [self orderViewsByImportance: views];
+//    NSUInteger totalWidthNeeded = [self calculateTotalWidthFromViews: sortedViews];
+//    
+//    CGRect frame = [view frame];
+//    [view setFrame: CGRectMake(frame.origin.x, frame.origin.y, totalWidthNeeded, frame.size.height)];
+//    
+//    [self layoutCloudsFromArray: sortedViews];
     
-    CGRect frame = [view frame];
-    [view setFrame: CGRectMake(frame.origin.x, frame.origin.y, totalWidthNeeded, frame.size.height)];
-    
-    [self layoutCloudsFromArray: sortedViews];
 //    [self layoutCloudsFromArrayNew: sortedViews];
 }
 
@@ -226,7 +227,34 @@
 // ------------------------------------------------------------
 - (void) cloudWindowOpened: (NSNotification*)notification
 {
-    NSLog(@"success!");
+    NSMutableArray* views = [[NSMutableArray alloc] init];
+    NSView* view = [self view];
+    
+    NSArray* topics = [notification object];
+    
+    for (NSUInteger i = 0; i < [topics count]; ++i)
+    {
+        NSDictionary* curTopic = [topics objectAtIndex: i];
+        NSString* curTopicString = [[curTopic allKeys] firstObject];
+        NSNumber* curTopicFrequency = [[curTopic allValues] firstObject];
+        
+        CETopic* topic = [[CETopic alloc] init];
+        [topic setTopicName: curTopicString];
+//        [topic setTopicRange: CMTimeRangeFromTimeToTime(CMTimeMake(100, 1), CMTimeMake(417, 1))];
+        [topic setImportanceWeighting: [curTopicFrequency integerValue]];
+        CECloudView* cloudView = [[CECloudView alloc] initWithTopic: topic];
+        [view addSubview: cloudView];
+        [views addObject: cloudView];
+    }
+    
+    NSArray* sortedViews = [self orderViewsByImportance: views];
+    NSUInteger totalWidthNeeded = [self calculateTotalWidthFromViews: sortedViews];
+    
+    CGRect frame = [view frame];
+    [view setFrame: CGRectMake(frame.origin.x, frame.origin.y, totalWidthNeeded, frame.size.height)];
+    
+    // [TODO] Clouds are being laid out in reverse order, fix.
+    [self layoutCloudsFromArray: sortedViews];
 }
 
 
